ea0a37029ac956192ac528b0abe462ce8305e340
Randall Reed, Jr <randallreedjr@gmail.com>
2016-10-06 14:29:44 -0400

    Move limit values to yaml file

--- lib/roth_ira.rb
+++ lib/roth_ira.rb
@@ -1,8 +1,11 @@
+require 'yaml'
+
 class RothIRA
-  attr_reader :year, :agi, :filing_status, :age, :spouse_age
+  attr_reader :age, :agi, :filing_status, :limits, :spouse_age, :year
 
   def initialize(year)
     @year = year
+    @limits = YAML.load(File.read(__dir__ + "/limits.yaml"))[year]
   end
 
   def calculate(agi, filing_status, *ages)
@@ -26,11 +29,11 @@ class RothIRA
   end
 
   def lower_limit
-    self.class.limits[year][filing_status][:lower_income_limit]
+    limits[filing_status][:lower_income_limit]
   end
 
   def upper_limit
-    self.class.limits[year][filing_status][:upper_income_limit]
+    limits[filing_status][:upper_income_limit]
   end
 
   def max_contribution
@@ -38,19 +41,19 @@ class RothIRA
   end
 
   def catch_up_age
-    self.class.limits[year][:catch_up_age]
+    limits[:catch_up_age]
   end
 
   def catch_up_contribution
-    self.class.limits[year][:catch_up_contribution]
+    limits[:catch_up_contribution]
   end
 
   def contribution_limit
-    self.class.limits[year][filing_status][:contribution_limit]
+    limits[filing_status][:contribution_limit]
   end
 
   def minimum_phase_out
-    self.class.limits[year][filing_status][:minimum_phase_out]
+    limits[filing_status][:minimum_phase_out]
   end
 
   def calculate_catch_up
@@ -85,54 +88,5 @@ class RothIRA
   def minimum_200(amount)
     # IRS instructions allow minimum contribution of $200 in phase-out range (per person)
     amount < minimum_phase_out ? minimum_phase_out : amount
-  end
-
-  def self.limits
-    { 
-      2015 => {
-        single: {
-          lower_income_limit: 116000,
-          upper_income_limit: 131000,
-          contribution_limit: 5500,
-          minimum_phase_out: 200
-        },
-        head_of_household: {
-          lower_income_limit: 116000,
-          upper_income_limit: 131000,
-          contribution_limit: 5500,
-          minimum_phase_out: 200
-        },
-        married_filing_jointly: {
-          lower_income_limit: 183000,
-          upper_income_limit: 193000,
-          contribution_limit: 11000,
-          minimum_phase_out: 400
-        },
-        catch_up_contribution: 1000,
-        catch_up_age: 50
-      },
-      2016 => {
-        single: {
-          lower_income_limit: 117000,
-          upper_income_limit: 132000,
-          contribution_limit: 5500,
-          minimum_phase_out: 200
-        },
-        head_of_household: {
-          lower_income_limit: 117000,
-          upper_income_limit: 132000,
-          contribution_limit: 5500,
-          minimum_phase_out: 200
-        },
-        married_filing_jointly: {
-          lower_income_limit: 184000,
-          upper_income_limit: 194000,
-          contribution_limit: 11000,
-          minimum_phase_out: 400
-        },
-        catch_up_contribution: 1000,
-        catch_up_age: 50
-      }
-    }
   end
 end
