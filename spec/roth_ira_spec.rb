a99b3f38c1e778493e046cc5f645c796b6984184
Randall Reed, Jr <randallreedjr@users.noreply.github.com>
2017-11-13 09:36:27 -0500

    Bump version number

--- spec/roth_ira_spec.rb
+++ spec/roth_ira_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe RothIRA do
   it 'should return correct version string' do
-    expect(RothIRA::VERSION).to eq('1.2.0')
+    expect(RothIRA::VERSION).to eq('1.2.1')
   end
 
   context 'Invalid tax years' do
